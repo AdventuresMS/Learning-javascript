a1f30f8feae2389a82d831ddaca0dd06ad8e4c20
Monika kumari <157510932+AdventuresMS@users.noreply.github.com>
2025-02-15 14:13:10 +0000

    container config

---
+++ myNewFolder/01_conversionOperation.js
@@ -0,0 +1,13 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(isLoggedIn)
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
